5d6a2037d45729e871d3fcdc7e7483f1c88e4efa
Mark D. Nagel <mnagel@willingminds.com>
2019-11-18 08:58:23 -0800

    Checkpoint from main branch.

--- WM/LMAPI.pm
+++ WM/LMAPI.pm
@@ -33,7 +33,6 @@ use HTTP::Request::Common;
 use LWP 5.8;
 use JSON;
 use YAML ();
-###use Hash::Merge qw(merge);
 use Time::HiRes qw(time);
 use Digest::SHA qw(hmac_sha256_hex);
 use MIME::Base64 ();
@@ -82,6 +81,18 @@ sub initialize {
     $self->{referbase} = "https://${company}.logicmonitor.com/santaba/uiv3";
 }
 
+sub get_one {
+    my $self = shift;
+    my %args = @_;
+    $args{size} = 1;
+
+    my $result = $self->get_all(%args);
+    if (ref $result eq 'ARRAY') {
+	return $result->[0];
+    }
+    return undef;
+}
+
 sub get_all {
     my $self = shift;
     my %args = @_;
@@ -92,32 +103,22 @@ sub get_all {
     delete $args{size};
 
     my $offset = 0;
+
     while (1) {
 	my $fetchsize = ((defined($maxitems) and $maxitems < $pagesize) ? $maxitems : $pagesize);
 	if (my $json = $self->_get(%args, size => $fetchsize, offset => $offset)) {
 	    if ($json->{status} == 200) {
-		if (exists $json->{data}->{items}) {
-		    if (ref $json->{data}->{items} eq 'ARRAY' and @{$json->{data}->{items}}) {
-			push(@$items, @{$json->{data}->{items}});
-		    }
-		    elsif (ref $json->{data}->{items} eq 'HASH') {
-			push(@$items, $json->{data}->{items});
-		    }
-		    else {
-			last;	# no more items
-		    }
+		if (my $jsonitems = _jsonitems($json)) {
+		    push(@$items, @{$jsonitems});
+		    # this should not be neededm but at least once we have
+		    # had items returned at offsets where they don't exist.
+		    last if (@$jsonitems < $pagesize);
 		}
-		elsif (exists $json->{items}) {
-		    if (ref $json->{items} eq 'ARRAY' and @{$json->{items}}) {
-			push(@$items, @{$json->{items}});
-		    }
-		    elsif (ref $json->{items} eq 'HASH') {
-			push(@$items, $json->{items});
-		    }
-		    else {
-			last;	# no more items
-		    }
+		else {
+		    last;	# no more items
 		}
+
+		# handle paging
 		if (defined($maxitems)) {
 		    if (($maxitems -= $fetchsize) <= 0) {
 			last;
@@ -130,11 +131,108 @@ sub get_all {
 	    }
 	}
 	else {
+	    # no results
 	    last;
 	}
     }
 
     return $items;
+}
+
+# similar to get_all, but for data (do not try to page, as it is very weird and undocumented)
+sub get_data_nonpaged {
+    my $self = shift;
+    my %args = @_;
+    my $items = [];
+
+    # normalize period to start/end (uses millsecond epoch, not properly documented)
+    my $etime;
+    my $stime;
+    if (defined $args{period}) {
+	$etime = int time;
+	$stime = $etime - (3600 * $args{period});
+    }
+    else {
+	$etime = $args{start};
+	$stime = $args{end};
+    }
+    croak "get_data: start time not defined" unless defined $stime;
+    croak "get_data: end time not defined" unless defined $etime;
+    croak "get_data: time warp (start after end)" if $stime > $etime;
+    $stime *= 1000;
+    $etime *= 1000;
+
+    # get one page of data, ignore the rest
+    if (my $json = $self->_get(%args, start => $stime, end => $etime)) {
+	if ($json->{status} == 200) {
+	    if (my $jsonitems = _jsonitems($json)) {
+		push(@$items, @{$jsonitems});
+	    }
+	}
+	else {
+	    croak "get_data: $json->{status} $json->{errmsg}\n" unless
+		$json->{status} == 1007		# Such datapoints([XXX]) do not belong to current datasource(ID=NNN).
+	     or $json->{status} == 1069;	# device<NNN> has no such DeviceDataSource
+	}
+    }
+
+    return $items;
+}
+
+sub _jsonitems {
+    my $json = shift;
+    my $items;
+
+    if (exists $json->{data}->{items}) {
+	if (ref $json->{data}->{items} eq 'ARRAY' and @{$json->{data}->{items}}) {
+	    push(@$items, @{$json->{data}->{items}});
+	}
+	elsif (ref $json->{data}->{items} eq 'HASH') {
+	    push(@$items, $json->{data}->{items});
+	}
+    }
+    elsif (exists $json->{items}) {
+	if (ref $json->{items} eq 'ARRAY' and @{$json->{items}}) {
+	    push(@$items, @{$json->{items}});
+	}
+	elsif (ref $json->{items} eq 'HASH') {
+	    push(@$items, $json->{items});
+	}
+    }
+    elsif (exists $json->{data}) {
+	if (ref $json->{data} eq 'HASH') {
+	    push(@$items, $json->{data});
+	}
+    }
+
+    return $items;
+}
+
+sub _version {
+    my $self = shift;
+    my %opts = @_;
+
+    my $version = $opts{'version'};
+    if (defined($version) and length($version)) {
+	$version = int($version);
+    }
+    else {
+	my $path = $opts{'path'};
+	if ($path =~ m:^(/setting/netscans/|/device/devices/\d+/flows|/setting/alert/internalalerts|/debug$):) {
+	    $version = 2;
+	}
+	elsif ($path =~ m:^/setting/(role|admin)/groups:) {
+	    $version = 3;
+	}
+	elsif ($path =~ m:^(/device/unmonitoreddevices)$:) {
+	    $version = 3;
+	}
+	else {
+	    $version = 1;
+	}
+    }
+
+    return $version;
 }
 
 # internal method, used by get_all to deal with paging
@@ -145,19 +243,8 @@ sub _get {
 
     my $path = $opts{'path'};
 
-    # extract explicit version, or set version implicitly for specific paths
-    my $version = $opts{'version'};
-    if (defined($version) and length($version)) {
-	$version = int($version);
-    }
-    else {
-	if ($path =~ m:^(/setting/netscans/|/device/devices/\d+/flows|/setting/alert/internalalerts):) {
-	    $version = 2;
-	}
-	else {
-	    $version = 1;
-	}
-    }
+    # set explicit version, or implicitly for known v2-only paths
+    my $version = $self->_version(%opts);
 
     for my $prop (qw(size sort filter fields offset format period datapoints)) {
         push(@args, "$prop=$opts{$prop}") if $opts{$prop};
@@ -205,9 +292,17 @@ sub _get {
 sub put {
     my $self = shift;
     my %opts = @_;
+    my @args;
 
     my $path = $opts{'path'};
     my $content = $opts{'content'};
+
+    # set explicit version, or implicitly for known v2-only paths
+    my $version = $self->_version(%opts);
+
+    for my $prop (qw(_scope collectorId)) {
+        push(@args, "$prop=$opts{$prop}") if $opts{$prop};
+    }
 
     # construct authorization string
     my $auth = $self->lmapiauth(method => 'PUT', %opts);
@@ -215,10 +310,26 @@ sub put {
     # setup HTTP agent and headers
     my $ua = LWP::UserAgent->new();
     my $url = $self->{baseurl} . $path;
-    my $req = PUT $url, 'Authorization' => $auth, 'Content-Type' => 'application/json', 'Content' => $content;
+    $url .= sprintf("?%s", join('&', @args)) if @args;
+    my @headers = (
+	'Authorization' => $auth,
+	'Content-Type' => 'application/json',
+	'Content' => $content,
+    );
+    if ($version > 1) {
+	push(@headers, "X-version" => "$version");
+    }
+
+    my $req = PUT $url, @headers;
     if (my $response = $ua->request( $req )) {
-	if (not $response->is_success) {
-	    carp "Response Status: ", $response->status_line. "\n";
+	if ($response->is_success) {
+	    return from_json($response->content);
+	}
+	else {
+	    carp "Problem with request:\n", 
+		 "    Request: $url\n", 
+		 "    Response Status: ", $response->status_line . "\n";
+	    return undef;
 	}
     }
 }
@@ -226,9 +337,17 @@ sub put {
 sub post {
     my $self = shift;
     my %opts = @_;
+    my @args;
 
     my $path = $opts{'path'};
     my $content = $opts{'content'};
+    #
+    # set explicit version, or implicitly for known v2-only paths
+    my $version = $self->_version(%opts);
+
+    for my $prop (qw(_scope collectorId)) {
+        push(@args, "$prop=$opts{$prop}") if $opts{$prop};
+    }
 
     # construct authorization string
     my $auth = $self->lmapiauth(method => 'POST', %opts);
@@ -236,13 +355,72 @@ sub post {
     # setup HTTP agent and headers
     my $ua = LWP::UserAgent->new();
     my $url = $self->{baseurl} . $path;
-    my $req = POST $url, 'Authorization' => $auth, 'Content-Type' => 'application/json', 'Content' => $content;
+    $url .= sprintf("?%s", join('&', @args)) if @args;
+    my @headers = (
+	'Authorization' => $auth,
+	'Content-Type' => 'application/json',
+	'Content' => $content,
+    );
+    if ($version > 1) {
+	push(@headers, "X-version" => "$version");
+    }
+
+    my $req = POST $url, @headers;
     if (my $response = $ua->request( $req )) {
 	if ($response->is_success) {
 	    return from_json($response->content);
 	}
 	else {
-	    carp "Response Status: ", $response->status_line. "\n";
+	    carp "Problem with request:\n", 
+		 "    Request: $url\n", 
+		 "    Response Status: ", $response->status_line . "\n";
+	    return undef;
+	}
+    }
+}
+
+sub patch {
+    my $self = shift;
+    my %opts = @_;
+    my @args;
+
+    my $path = $opts{'path'};
+    my $content = $opts{'content'};
+    #
+    # set explicit version, or implicitly for known v2-only paths
+    my $version = $self->_version(%opts);
+
+    for my $prop (qw(_scope collectorId patchFields)) {
+        push(@args, "$prop=$opts{$prop}") if $opts{$prop};
+    }
+
+    # construct authorization string
+    my $auth = $self->lmapiauth(method => 'PATCH', %opts);
+
+    # setup HTTP agent and headers
+    my $ua = LWP::UserAgent->new();
+    my $url = $self->{baseurl} . $path;
+    $url .= sprintf("?%s", join('&', @args)) if @args;
+    my @headers = (
+	'Authorization' => $auth,
+	'Content-Type' => 'application/json',
+    );
+    if ($version > 1) {
+	push(@headers, "X-version" => "$version");
+    }
+
+    # LWP versions currently deployed don't natively support
+    # HTTP 'PATCH'.
+    my $req = HTTP::Request->new('PATCH', $url, \@headers, $content);
+    if (my $response = $ua->request( $req )) {
+	if ($response->is_success) {
+	    return from_json($response->content);
+	}
+	else {
+	    carp "Problem with request:\n", 
+		 "    Request: $url\n", 
+		 "    Response Status: ", $response->status_line . "\n";
+	    return undef;
 	}
     }
 }
@@ -255,7 +433,7 @@ sub lmapiauth {
     my $method = $opts{'method'};
     my $content = "";
 
-    $content = $opts{'content'} if ($method eq "PUT" or $method eq "POST");
+    $content = $opts{'content'} if ($method eq "PUT" or $method eq "POST" or $method eq "PATCH");
     
     # construct authorization string
     my $epoch = int(time * 1000);	# time in ms
